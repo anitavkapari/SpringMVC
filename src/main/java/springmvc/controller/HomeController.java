74240bc235ea0cf53a8126031164d9d1149947b7
anitavkapari <anitavkapari@gmail.com>
2022-02-02 20:20:12 +0530

    model controller use list view and string

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -1,14 +1,29 @@
 package springmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 		
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
         System.out.println("this our home page");
+        model.addAttribute("name", "Anita Kapari");
+        model.addAttribute("id", 1222);
+        List<String> frnds = new ArrayList<String>();
+        frnds.add("Anita");
+        frnds.add("Annu");
+        frnds.add("Avi");
+        frnds.add("Aniket");
+        frnds.add("Ankita");
+        frnds.add("Anita Kapari");
+
+        model.addAttribute("f", frnds);
 		return "index";
 	}
 	
@@ -17,4 +32,5 @@ public class HomeController {
         System.out.println("this our about page");
 		return "about";
 	}
+	
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@page import="java.util.*"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,6 +9,24 @@
 </head>
 <body>
 	<h1>This is home page</h1>
-	<h1>url/call</h1>
+	<%
+		String name = (String) request.getAttribute("name");
+		Integer id = (Integer) request.getAttribute("id");
+		List<String> friend = (List<String>) request.getAttribute("f");
+	%>
+	<h1>Name is <%=name %></h1>
+	<h1>ID is <%=id %></h1>
+	
+	
+	<% 
+	for(String s : friend) {
+/* 	 out.println(s);
+ */
+	%>
+		<h1> <%=s %></h1>
+	<%
+	}
+	%>
+	
 </body>
 </html>
